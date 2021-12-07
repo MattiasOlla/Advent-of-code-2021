6f46c67cc1080a7c75c6259904e2cd5a6e2f0c08
Mattias Olla <mattias.olla@gmail.com>
2021-12-07 08:05:12 +0100

    Solution day 7
    This feels like it should be solvable mathematically...

---
+++ day7.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from typing import Callable, Generator
+
+filepath = Path(__file__)
+data_path = filepath.parent / "data" / f"{filepath.stem}.txt"
+data = [int(x) for x in data_path.read_text().split(",")]
+
+
+def distance_gen(
+    level: int, func: Callable[[int], int] = lambda x: x
+) -> Generator[int, None, None]:
+    for x in data:
+        yield func(abs(x - level))
+
+
+def part1():
+    print(min((sum(distance_gen(level)), level) for level in range(min(data), max(data) + 1)))
+
+
+def part2():
+    print(
+        min(
+            (sum(distance_gen(level, lambda x: x * (x + 1) // 2)), level)
+            for level in range(min(data), max(data) + 1)
+        )
+    )
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
