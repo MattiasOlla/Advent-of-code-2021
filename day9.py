0b27a00f1984782eef49d725a58bf178a88f08b7
Mattias Olla <mattias.olla@gmail.com>
2021-12-09 09:13:44 +0100

    WIP

--- day9.py
+++ day9.py
@@ -1,5 +1,7 @@
+from functools import reduce
+from operator import mul
 from pathlib import Path
-from tkinter.messagebox import NO
+from pprint import pprint
 from typing import Generator
 
 filepath = Path(__file__)
@@ -7,25 +9,60 @@ data_path = filepath.parent / "data" / f"{filepath.stem}.txt"
 data = [[int(x) for x in row] for row in data_path.read_text().splitlines()]
 
 
-def neighbour_gen(data: list[list[int]], row: int, col: int) -> Generator[int, None, None]:
+def neighbour_gen(
+    data: list[list[int]], row: int, col: int
+) -> Generator[tuple[int, int], None, None]:
     num_rows = len(data)
     num_cols = len(data[row])
 
     for n_row, n_col in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
         if 0 <= n_row < num_rows and 0 <= n_col < num_cols:
-            yield data[n_row][n_col]
+            yield n_row, n_col
+
+
+def find_low_points(data: list[list[int]]) -> list[tuple[int, int]]:
+    return [
+        (i, j)
+        for i, row in enumerate(data)
+        for j, height in enumerate(row)
+        if all(height < data[r][c] for r, c in neighbour_gen(data, i, j))
+    ]
 
 
 def part1():
-    low_point_heights = [
-        height
-        for i, row in enumerate(data)
-        for j, height in enumerate(row)
-        if all(height < neighbour for neighbour in neighbour_gen(data, i, j))
-    ]
+    low_point_heights = [data[i][j] for i, j in find_low_points(data)]
     risk_level = sum(low_point_heights) + len(low_point_heights)
     print(f"{risk_level=}")
 
 
+def find_baisin_size(data: list[list[int]], row: int, col: int) -> int:
+    # pprint(
+    #     [
+    #         data_row[max(col - 6, 0) : min(col + 7, len(data_row))]
+    #         for data_row in data[max(row - 6, 0) : min(row + 7, len(data))]
+    #     ]
+    # )
+    baisin_coords = set()
+    queue = [(row, col)]
+    while queue:
+        curr_row, curr_col = queue.pop(0)
+        queue.extend(
+            (neigh_row, neigh_col)
+            for neigh_row, neigh_col in set(neighbour_gen(data, curr_row, curr_col)) - baisin_coords
+            if data[curr_row][curr_col] < data[neigh_row][neigh_col]
+        )
+        baisin_coords.add((curr_row, curr_col))
+
+    return len(baisin_coords)
+
+
+def part2():
+    low_point_idx = find_low_points(data)
+    baisin_sizes = [find_baisin_size(data, i, j) for i, j in low_point_idx]
+    product = reduce(mul, sorted(baisin_sizes, reverse=True)[:3])
+    print(f"{product=}")
+
+
 if __name__ == "__main__":
     part1()
+    part2()
