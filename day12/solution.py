4ca897b6d3f27bddfa144d09415eef1bb0dee85c
Mattias Olla <mattias.olla@gmail.com>
2021-12-12 13:50:48 +0100

    Solution day 12

---
+++ day12/solution.py
@@ -0,0 +1,118 @@
+from collections import defaultdict
+from copy import deepcopy
+from dataclasses import dataclass, field
+from pathlib import Path
+from textwrap import dedent
+from typing import Any, Generator, TypeAlias
+
+DataType: TypeAlias = dict[str, "Cave"]
+
+data_path = Path(__file__).parent / "data.txt"
+
+
+@dataclass
+class Cave:
+    name: str
+    small: bool
+
+    neighbours: list["Cave"] = field(default_factory=list)
+
+    @classmethod
+    def from_name(cls, name: str) -> "Cave":
+        return cls(name=name, small=name.islower())
+
+
+def parse_data(text: str) -> DataType:
+    caves = {}
+    for line in text.splitlines():
+        name1, _, name2 = line.partition("-")
+        cave1 = caves.setdefault(name1, Cave.from_name(name1))
+        cave2 = caves.setdefault(name2, Cave.from_name(name2))
+        cave1.neighbours.append(cave2)
+        cave2.neighbours.append(cave1)
+    return caves
+
+
+class CavePath:
+    def __init__(self, path: list[Cave]) -> None:
+        self.path: list[Cave] = []
+        self.visited: defaultdict[str, int] = defaultdict(int)
+        for cave in path:
+            self.append(cave)
+
+        self.allowed_twice = ""
+
+    @property
+    def last(self) -> Cave:
+        return self.path[-1]
+
+    def append(self, cave: Cave) -> None:
+        self.path.append(cave)
+        if cave.small:
+            self.visited[cave.name] += 1
+
+    def __str__(self) -> str:
+        return ",".join(cave.name for cave in self.path)
+
+    def copy(self) -> "CavePath":
+        new = CavePath(self.path.copy())
+        new.allowed_twice = self.allowed_twice
+        return new
+
+
+def find_paths(
+    caves: dict[str, Cave],
+    start_name: str = "start",
+    end_name: str = "end",
+) -> Generator[CavePath, None, None]:
+    queue = [CavePath([caves[start_name]])]
+    while queue:
+        curr = queue.pop(0)
+        if curr.last.name == end_name:
+            yield curr
+            continue
+        for neighbour in curr.last.neighbours:
+            times_visited = curr.visited.get(neighbour.name, 0)
+            if times_visited == 0:
+                new = curr.copy()
+                new.append(neighbour)
+                queue.append(new)
+
+
+def find_paths_allow_one_twice(
+    caves: dict[str, Cave],
+    start_name: str = "start",
+    end_name: str = "end",
+) -> Generator[CavePath, None, None]:
+    queue = [CavePath([caves[start_name]])]
+    while queue:
+        curr = queue.pop(0)
+        if curr.last.name == end_name:
+            yield curr
+            continue
+        for neighbour in curr.last.neighbours:
+            times_visited = curr.visited.get(neighbour.name, 0)
+            if times_visited == 0 or (
+                times_visited == 1
+                and not curr.allowed_twice
+                and neighbour.name not in {start_name, end_name}
+            ):
+                new = curr.copy()
+                if times_visited == 1:
+                    new.allowed_twice = neighbour.name
+                new.append(neighbour)
+                queue.append(new)
+
+
+def part1(data: DataType) -> int:
+    return len(list(find_paths(data)))
+
+
+def part2(data: DataType) -> int:
+    return len(set(find_paths_allow_one_twice(data)))
+
+
+if __name__ == "__main__":
+    data = parse_data(data_path.read_text())
+    print(f"Part 1 result: {part1(data)}")
+    print(f"Part 2 result: {part2(data)}")
---
+++ day12/test_solution.py
@@ -0,0 +1,153 @@
+from textwrap import dedent
+from typing import Counter
+
+from solution import find_paths, find_paths_allow_one_twice, parse_data, part1, part2
+
+test_data_str = dedent(
+    """
+    dc-end
+    HN-start
+    start-kj
+    dc-start
+    dc-HN
+    LN-dc
+    HN-end
+    kj-sa
+    kj-HN
+    kj-dc
+    """
+).strip()
+test_data = parse_data(test_data_str)
+
+test_data_str_short = dedent(
+    """
+    start-A
+    start-b
+    A-c
+    A-b
+    b-d
+    A-end
+    b-end
+    """
+).strip()
+test_data_short = parse_data(test_data_str_short)
+
+
+test_data_long = parse_data(
+    dedent(
+        """
+        fs-end
+        he-DX
+        fs-he
+        start-DX
+        pj-DX
+        end-zg
+        zg-sl
+        zg-pj
+        pj-he
+        RW-he
+        fs-DX
+        pj-RW
+        zg-RW
+        start-pj
+        he-WI
+        zg-he
+        pj-fs
+        start-RW
+        """
+    ).strip()
+)
+
+
+def test_find_paths():
+    paths = set(str(p) for p in find_paths(test_data))
+    assert paths == {
+        "start,HN,dc,HN,end",
+        "start,HN,dc,HN,kj,HN,end",
+        "start,HN,dc,end",
+        "start,HN,dc,kj,HN,end",
+        "start,HN,end",
+        "start,HN,kj,HN,dc,HN,end",
+        "start,HN,kj,HN,dc,end",
+        "start,HN,kj,HN,end",
+        "start,HN,kj,dc,HN,end",
+        "start,HN,kj,dc,end",
+        "start,dc,HN,end",
+        "start,dc,HN,kj,HN,end",
+        "start,dc,end",
+        "start,dc,kj,HN,end",
+        "start,kj,HN,dc,HN,end",
+        "start,kj,HN,dc,end",
+        "start,kj,HN,end",
+        "start,kj,dc,HN,end",
+        "start,kj,dc,end",
+    }
+
+
+def test_find_paths_allowed_twice_short():
+    paths = list(find_paths_allow_one_twice(test_data_short))
+    paths_str = set(str(p) for p in paths)
+    expected = {
+        "start,A,b,A,b,A,c,A,end",
+        "start,A,b,A,b,A,end",
+        "start,A,b,A,b,end",
+        "start,A,b,A,c,A,b,A,end",
+        "start,A,b,A,c,A,b,end",
+        "start,A,b,A,c,A,c,A,end",
+        "start,A,b,A,c,A,end",
+        "start,A,b,A,end",
+        "start,A,b,d,b,A,c,A,end",
+        "start,A,b,d,b,A,end",
+        "start,A,b,d,b,end",
+        "start,A,b,end",
+        "start,A,c,A,b,A,b,A,end",
+        "start,A,c,A,b,A,b,end",
+        "start,A,c,A,b,A,c,A,end",
+        "start,A,c,A,b,A,end",
+        "start,A,c,A,b,d,b,A,end",
+        "start,A,c,A,b,d,b,end",
+        "start,A,c,A,b,end",
+        "start,A,c,A,c,A,b,A,end",
+        "start,A,c,A,c,A,b,end",
+        "start,A,c,A,c,A,end",
+        "start,A,c,A,end",
+        "start,A,end",
+        "start,b,A,b,A,c,A,end",
+        "start,b,A,b,A,end",
+        "start,b,A,b,end",
+        "start,b,A,c,A,b,A,end",
+        "start,b,A,c,A,b,end",
+        "start,b,A,c,A,c,A,end",
+        "start,b,A,c,A,end",
+        "start,b,A,end",
+        "start,b,d,b,A,c,A,end",
+        "start,b,d,b,A,end",
+        "start,b,d,b,end",
+        "start,b,end",
+    }
+    assert paths_str == expected
+
+
+def test_find_paths_allowed_twice():
+    paths = list(find_paths_allow_one_twice(test_data))
+    # for path in paths:
+    #     print(path)
+    #     print(Counter([c.name for c in path.path]))
+    # print(len(set("".join(sorted(c.name for c in path.path)) for path in paths)))
+    assert len(paths) == 103
+
+
+def test_find_paths_allowed_twice_long():
+    paths = list(find_paths_allow_one_twice(test_data_long))
+    for path in paths:
+        print(Counter([c.name for c in path.path]))
+
+    assert len(set(paths)) == 3509
+
+
+def test_part1():
+    assert part1(test_data) == 19
+
+
+def test_part2():
+    assert part2(test_data) == 103
